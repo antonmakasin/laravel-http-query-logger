d3aa21bfc9ed5ee8230f29d58516d09693b0dd19
Vlad Oskin <oskinvv95@gmail.com>
2020-05-22 12:35:02 +0700

    Fix mail styles

--- src/resources/views/mail/notification.blade.php
+++ src/resources/views/mail/notification.blade.php
@@ -2,7 +2,7 @@
 @foreach($fields as $name => $value)
     <tr>
         <td style="padding: 10px; border: #e9e9e9 1px solid;"><p>{{ $name }}</p></td>
-        <td><p>{{ $value }}</p></td>
+        <td style="padding: 10px; border: #e9e9e9 1px solid;background-color: #f8f8f8;"><p>{{ $value }}</p></td>
     </tr>
 @endforeach
 </table>
